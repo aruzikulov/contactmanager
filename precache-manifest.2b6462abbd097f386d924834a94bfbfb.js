a847d1e4769548f75822877c4299886897c4793b
Akhmadjon Ruzikulov <aruzikulov@hust.edu.cn>
2018-11-13 19:46:11 +0800

    Updates

---
+++ precache-manifest.2b6462abbd097f386d924834a94bfbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "da36943e414e92c4502b",
+    "url": "/contactmanager/static/js/main.da36943e.chunk.js"
+  },
+  {
+    "revision": "7ff017e2d4991117ccdd",
+    "url": "/contactmanager/static/js/1.7ff017e2.chunk.js"
+  },
+  {
+    "revision": "da36943e414e92c4502b",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "7ff017e2d4991117ccdd",
+    "url": "/contactmanager/static/css/1.1809f380.chunk.css"
+  },
+  {
+    "revision": "535c1ae13c55c745298ff1f18031368a",
+    "url": "/contactmanager/index.html"
+  }
+];
